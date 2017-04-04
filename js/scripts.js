076d91a09f57b73eb417d455dfb3742718173cde
Emily Dokken and Jessica Robertson <jessica@>
2017-04-03 20:50:29 -0700

    build weeklyBreakdown constructor and prototype to push daily total tips.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,35 @@
 //Back-End Logic
-// tilTotal - 300 = actualCash
-//
-// posCash - actualCash = difference
-//
-// cashTips + ccTips = totalTips
-//
-// actualCash - ccTips = drop
-
-// function Breakdown(tilTotal, posCash, cashTips, ccTips) {
-//   this.tilTotal = tilTotal;
-//   this.posCash = posCash;
-//   this.cashTips = cashTips;
-//   this.ccTips = ccTips;
-// }
-//
-// breakdown.prototype.cashMath = function() {
-//   return this.tilTotal - 300;
-// }
-//
-// breakdown.prototype.cashDifference = function() {
-//   return ac
-// }
 
 var actualCashMath = function(tilTotal) {
   return tilTotal - 300;
 };
 
+function weeklyBreakdown(monday, tuesday, wednesday, thursday, friday, saturday, sunday) {
+  this.monday = monday;
+  this.tuesday = tuesday;
+  this.wednesday = wednesday;
+  this.thursday = thursday;
+  this.friday = friday;
+  this.saturday = saturday;
+  this.sunday = sunday;
+  this.week = [];
+}
 
+weeklyBreakdown.prototype.tipPush = function() {
+  if (weekday === 1) {
+    this.monday = totalTips;
+  } else if (weekday === 2) {
+    this.tuesday === totalTips;
+  } else if (weekday === 3) {
+    this.wednesday === totalTips;
+  } else if (weekday === 4) {
+    this.thursday === totalTips;
+  } else if (weekday === 5) {
+    this.friday === totalTips;
+  } else if (weekday === 6) {
+    this.saturday === totalTips;
+  } else (weekday === 7)
+}
 
 
 //Front-End Logic
@@ -34,23 +37,25 @@ $(document).ready(function () {
   $("form").submit(function(event) {
     event.preventDefault();
 
+    var weekday = $("select#weekdays").val();
     var tilTotal = parseInt($('input#tilTotal').val());
+    var posCash = parseInt($('input#posCash').val());
+    var cashTips = parseInt($('input#cashTips').val());
+    var ccTips = parseInt($('input#ccTips').val());
+
     var actualCash = actualCashMath(tilTotal);
     $("#actualCash").text(actualCash);
 
-    var posCash = parseInt($('input#posCash').val());
-
     var difference = actualCash - posCash
     $("#difference").text(difference);
-
-    var cashTips = parseInt($('input#cashTips').val());
-    var ccTips = parseInt($('input#ccTips').val());
 
     var totalTips = cashTips + ccTips
     $("#totalTips").text(totalTips);
 
     var Drop = actualCash - ccTips
     $("#drop").text(Drop);
+
+
 
   });
 });
